40ca28d5828f5b67cb9ce5d96bd7b675a4c98d4b
Thanh Nhien <nhien.nguyenthanh@gmail.com>
2015-10-06 08:18:45 +0700

    Change Register.java

---
+++ app/src/main/java/nguyenthanhnhien/smartdog/app/AppConfig.java
@@ -0,0 +1,12 @@
+package nguyenthanhnhien.smartdog.app;
+
+/**
+ * Created by Nguyễn Thanh Nhiên on 9/28/2015.
+ */
+public class AppConfig {
+    // Server user login url
+    public static String URL_LOGIN = "http://172.29.98.30/smartdog/login.php";
+
+    // Server user register url
+    public static String URL_REGISTER = "http://172.29.98.30/smartdog/register.php";
+}
